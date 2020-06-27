e98b9e45a077f3ee896b08cb2769d1cb3721b0d8
Lisa Feng <lisafeng10@gmail.com>
2020-06-27 18:41:50 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+require "pry"
 
+class CashRegister
+  attr_accessor :total, :discount
+  
+  
+  def initialize(total = 0, discount = 20)
+    @total = total
+    @discount = discount
+  end
+  
+  def add_item(title, price, quantity = 1)
+    self.total += quantity*price
+  end
+  
+  def apply_discount
+    self.total -= self.total*(self.discount.to_f/100.to_f)
+  end
+  
+end
